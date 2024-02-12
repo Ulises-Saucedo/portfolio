72b49932c75cf1037d25629ca5f3a64c6957bd92
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-12 02:39:28 -0300

    presentation updated

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,150 +1,53 @@
 <script setup>
-const emits = defineEmits(["toggle"]);
-const props = defineProps({
-  isDark: {
-    type: Boolean,
-  },
-});
+import { inject } from "vue";
+import SocialMedia from "./SocialMedia.vue";
 
-const toggleTheme = () => {
-  emits("toggle");
-};
+const socialMedia = [
+  {
+    name: "Github",
+    src: "/icons/github.svg",
+    href: "https://github.com/Ulises-Saucedo/",
+  },
+  {
+    name: "LinkedIn",
+    src: "/icons/linkedin.svg",
+    href: "https://www.linkedin.com/in/ulises-saucedo/",
+  },
+  {
+    name: "Gmail",
+    src: "/icons/gmail.svg",
+    href: "mailto:ulisesaucedo.se04@gmail.com",
+  },
+];
+
+const isDark = inject("isDark");
 </script>
 
 <template>
-  <section class="main-subsection" :class="{ 'dark-mode': props.isDark }">
-    <div class="toggle-theme">
-      <button @click="toggleTheme">
-        <img
-          :src="props.isDark ? '/icons/moon.svg' : '/icons/sun.svg'"
-          :alt="props.isDark ? 'moon' : 'sun'"
-        />
-      </button>
-    </div>
-    <h3 class="main-subtitle">Hey 👋, soy Ulises</h3>
-    <h1 class="main-title">Desarrollador trainee</h1>
-    <p class="main-description">
-      En 2023 me egresé de una escuela secundaria técnica, la cual en las
-      prácticas profesionalizantes me dio la posibilidad de desarrollar tanto
-      habilidades técnicas como blandas.
+  <section class="mx-auto my-24 max-w-[800px] p-4">
+    <h3
+      class="font-clashDisplayLight text-h3 text-[#2F3037]"
+      :class="{ 'text-[#fff]': isDark }"
+    >
+      Hey 👋, soy
+    </h3>
+    <h1
+      class="font-clashDisplaySemibold text-[48px] text-[#2F3037]"
+      :class="{ 'text-[#fff]': isDark }"
+    >
+      Ulises Saucedo
+    </h1>
+    <p
+      class="font-authorRegular text-[18px] text-[#2F3037]"
+      :class="{ 'text-[#fff]': isDark }"
+    >
+      Comencé mi viaje en la programación a los 15 años, inmerso en una
+      tecnicatura que ha sido fundamental en mi formación. Enfrenté diversos
+      desafíos que contribuyeron al desarrollo de mis habilidades y
+      conocimientos.
     </p>
-  </section>
-  <section class="subsection-buttons">
-    <a href="https://github.com/Ulises-Saucedo/" target="_blank">
-      Github
-      <img src="/icons/github.svg" alt="github" />
-    </a>
-    <a href="https://www.linkedin.com/in/ulises-saucedo/" target="_blank"
-      >LinkedIn
-      <img src="/icons/linkedin.svg" alt="github" />
-    </a>
-    <a href="mailto:ulisesaucedo.se04@gmail.com" target="_blank"
-      >ulisesaucedo.se04@gmail.com
-      <img src="/icons/gmail.svg" alt="github" />
-    </a>
+    <section class="flex flex-wrap justify-start gap-4 my-4 font-authorRegular">
+      <SocialMedia v-for="media in socialMedia" :socialMedia="media" />
+    </section>
   </section>
 </template>
-
-<style scoped>
-.dark-mode .main-title,
-.dark-mode .main-subtitle,
-.dark-mode .main-description {
-  color: #fff;
-}
-.toggle-theme {
-  display: flex;
-  justify-content: flex-end;
-  width: 100%;
-}
-.toggle-theme button {
-  border: none;
-  background: transparent;
-  cursor: pointer;
-}
-.main-subsection {
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-}
-.subsection-buttons {
-  display: flex;
-  gap: 24px;
-}
-.main-title {
-  font-family: "Clash Display Semibold";
-  font-size: var(--text-h1);
-  color: var(--dark-100);
-}
-.main-subtitle {
-  display: flex;
-  font-family: "Clash Display Light";
-  font-size: var(--text-h3);
-  color: var(--dark-100);
-}
-.main-description {
-  font-family: "Satoshi Regular";
-  font-size: var(--text-base);
-  color: var(--dark-100);
-}
-.subsection-buttons a {
-  display: flex;
-  align-items: center;
-  gap: 24px;
-  padding: 16px;
-  background: var(--primary-gradient);
-  color: #fff;
-  border-radius: 8px;
-  cursor: pointer;
-  font-family: "Clash Display Semibold";
-  font-size: var(--text-h6);
-  text-decoration: none;
-}
-@media screen and (max-width: 1200px) {
-  .main-subtitle {
-    font-size: var(--text-h4);
-  }
-  .main-title {
-    font-size: var(--text-h3);
-  }
-  .subsection-buttons {
-    gap: 8px;
-  }
-  .subsection-buttons a {
-    gap: 8px;
-    padding: 8px;
-    font-size: var(--text-base);
-  }
-}
-@media screen and (max-width: 800px) {
-  .main-subtitle {
-    font-size: var(--text-h5);
-  }
-  .main-title {
-    font-size: var(--text-h4);
-  }
-  .subsection-buttons {
-    gap: 8px;
-  }
-  .subsection-buttons a {
-    gap: 8px;
-    padding: 8px;
-    font-size: var(--text-base);
-  }
-  .subsection-buttons a img {
-    width: 20px;
-    height: 20px;
-  }
-}
-@media screen and (max-width: 600px) {
-  .subsection-buttons {
-    flex-wrap: wrap;
-  }
-  .subsection-buttons a {
-    font-size: var(--text-small);
-  }
-  .subsection-buttons a img {
-    width: 15px;
-    height: 15px;
-  }
-}
-</style>
