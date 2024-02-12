8a0dd67e517242e8c54d088a4606e0a97537ec90
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-12 05:07:07 -0300

    update main view

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { provide } from "vue";
 import { useDark, useToggle } from "@vueuse/core";
 import Presentation from "./components/Presentation.vue";
 import Proyectos from "./components/Proyectos.vue";
@@ -11,16 +12,17 @@ const isDark = useDark({
   valueDark: "dark",
   valueLight: "light",
 });
+
 const toggleDark = useToggle(isDark);
+
+provide("isDark", isDark);
 </script>
 
 <template>
-  <section class="main-section">
-    <Presentation @toggle="toggleDark" :isDark="isDark" />
-    <Proyectos :isDark="isDark" />
-    <AboutMe :isDark="isDark" />
-  </section>
-  <Footer :isDark="isDark" />
+  <Presentation @toggleTheme="toggleDark" />
+  <Proyectos />
+  <AboutMe />
+  <Footer />
 </template>
 
 <style>
@@ -28,20 +30,6 @@ const toggleDark = useToggle(isDark);
   background: #242424;
 }
 [color-scheme="light"] {
-  background: #ccd5ae;
-}
-.main-section {
-  margin: 256px auto;
-  display: flex;
-  flex-direction: column;
-  padding: 16px;
-  gap: 32px;
-  max-width: 900px;
-}
-@media screen and (max-width: 1400px) {
-  .main-section {
-    margin: 96px auto;
-    max-width: 800px;
-  }
+  background: #fff;
 }
 </style>
