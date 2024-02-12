08e16e25a5ed2fc593be4442c36f03d0bb5f9f39
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-12 04:36:33 -0300

    project card updated

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,47 +1,55 @@
 <script setup>
+import { inject } from "vue";
 const props = defineProps({
-  image: {
-    type: String,
-  },
-  title: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  images: {
-    type: Array,
-  },
-  links: {
-    type: Array,
-  },
-  isDark: {
-    type: Boolean,
+  project: {
+    type: Object,
   },
 });
+
+const { title, description, technologies, links, image } = props.project;
+const isDark = inject("isDark");
 </script>
 
 <template>
-  <div class="card-container" :class="{ 'dark-mode': props.isDark }">
-    <img :src="props.image" :alt="props.title" class="preview" />
-    <div class="card-content">
-      <div class="card-header">
-        <h3 class="card-title">{{ props.title }}</h3>
-        <p class="card-description">{{ props.description }}</p>
-        <div class="tecnologies-container">
+  <div class="overflow-hidden">
+    <img :src="image" :alt="title" class="rounded-xl" />
+    <div>
+      <div>
+        <h3
+          class="py-1 font-clashDisplayMedium text-h3 text-[#2F3037]"
+          :class="{ 'text-[#fff]': isDark }"
+        >
+          {{ title }}
+        </h3>
+        <p
+          class="py-1 font-author text-[18px] text-[#2F3037]"
+          :class="{ 'text-[#fff]': isDark }"
+        >
+          {{ description }}
+        </p>
+        <div class="py-1 flex flex-wrap gap-2">
           <img
             :src="image.src"
             :alt="image.alt"
-            v-for="image in props.images"
-            class="card-technologies"
+            v-for="image in technologies"
+            class="py-2 px-4 rounded-md"
+            :class="{
+              'bg-[#E7FDF9]': !isDark,
+              'bg-[#065042]': isDark,
+            }"
           />
         </div>
       </div>
-      <div class="card-footer">
+      <div class="my-3 flex gap-2">
         <a
           :href="link.href"
-          v-for="link in props.links"
-          :style="{ width: `${100 / props.links.length}%` }"
+          v-for="link in links"
+          :style="{ width: `${100 / links.length}%` }"
+          class="flex justify-center ease-in-out duration-300 border-[1px] border-solid border-primary-hover text-[18px] text-primary-hover py-1 rounded-2xl"
+          :class="{
+            'hover:bg-[#E7FDF9]': !isDark,
+            'hover:bg-[#065042]': isDark,
+          }"
         >
           {{ link.title }}
         </a>
@@ -49,111 +57,3 @@ const props = defineProps({
     </div>
   </div>
 </template>
-
-<style scoped>
-.dark-mode .card-title,
-.dark-mode .card-description {
-  color: #fff;
-}
-.dark-mode .card-technologies {
-  background: var(--dark-50);
-}
-.card-container {
-  display: flex;
-  gap: 32px;
-}
-.preview {
-  max-width: 500px;
-  max-height: 400px;
-  object-fit: cover;
-  border-radius: 8px;
-}
-.card-content {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-.card-header {
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-}
-.card-title {
-  color: var(--dark-100);
-  font-size: var(--text-h4);
-  font-family: "Clash Display Semibold";
-}
-.card-description {
-  color: var(--dark-100);
-  font-size: var(--text-base);
-  font-family: "Satoshi Regular";
-}
-.card-footer {
-  display: flex;
-  width: 100%;
-  gap: 10px;
-}
-.tecnologies-container {
-  display: flex;
-  gap: 8px;
-}
-.card-technologies {
-  padding: 8px;
-  border-radius: 4px;
-  background: var(--dark-10);
-  width: 40px;
-  height: 40px;
-}
-.card-footer a {
-  color: #fff;
-  font-size: var(--text-h6);
-  font-family: "Clash Display Semibold";
-  text-decoration: none;
-  padding: 8px 16px;
-  background: var(--primary-gradient);
-  width: min-content;
-  text-align: center;
-  border-radius: 10px;
-}
-@media screen and (max-width: 1400px) {
-  .preview {
-    max-width: 400px;
-    border-radius: 8px;
-  }
-  .card-container {
-    gap: 16px;
-  }
-  .card-header {
-    gap: 8px;
-  }
-  .card-title {
-    font-size: var(--text-h5);
-  }
-  .card-footer a {
-    font-size: var(--text-base);
-  }
-}
-@media screen and (max-width: 800px) {
-  .card-container {
-    display: grid;
-    max-width: 500px;
-    grid-template-columns: minmax(150px, 1fr);
-    margin: 0 auto;
-  }
-  .preview {
-    max-width: 100%;
-    object-fit: cover;
-    display: block;
-  }
-  .card-content {
-    gap: 16px;
-  }
-  .card-footer a {
-    font-size: var(--text-small);
-  }
-  .card-technologies {
-    width: 35px;
-    height: 35px;
-  }
-}
-</style>
