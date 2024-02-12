39b978b858abb3c11e52c28ac6decbfe1f13bb8b
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-12 04:43:53 -0300

    about section updated

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,21 +1,30 @@
 <script setup>
-const props = defineProps({
-  isDark: {
-    type: Boolean,
-  },
-});
+import { inject } from "vue";
+
+const isDark = inject("isDark");
 </script>
 
 <template>
-  <section class="about-container" :class="{ 'dark-mode': props.isDark }">
-    <h2 class="about-title">Sobre mí</h2>
-    <p>
+  <section class="flex flex-col gap-8 max-w-[1000px] p-8 mx-auto">
+    <h2
+      class="font-clashDisplayMedium text-h3 text-[#2F3037] text-center mt-32"
+      :class="{ 'text-[#fff]': isDark }"
+    >
+      Sobre mí
+    </h2>
+    <p
+      class="font-authorRegular text-[18px] text-balance text-pretty text-[#2F3037]"
+      :class="{ 'text-[#fff]': isDark }"
+    >
       Mi nombre completo es Ulises Saucedo. Empecé en la programación a los 15
       años cuando decidí sumergirme en la tecnicatura que ha marcado mi
       formación. A lo largo del trayecto he enfrentado diversos retos que han
       contribuido a la forja de mis habilidades y conocimientos.
     </p>
-    <p>
+    <p
+      class="font-authorRegular text-[18px] text-balance text-pretty text-[#2F3037]"
+      :class="{ 'text-[#fff]': isDark }"
+    >
       Mi graduación en 2023 a los 19 años fue el resultado del compromiso con mi
       desarrollo profesional, especialmente durante el último año de la
       tecnicatura, donde las prácticas profesionalizantes se convirtieron en el
@@ -23,38 +32,13 @@ const props = defineProps({
       consolidé mis conocimientos, sino que también tuve la oportunidad de
       aplicarlos en situaciones del mundo real.
     </p>
-    <p>
+    <p
+      class="font-authorRegular text-[18px] text-balance text-pretty text-[#2F3037]"
+      :class="{ 'text-[#fff]': isDark }"
+    >
       En mi caja de herramientas, destacan HTML, CSS, JS, VUE y bases de datos
       tanto SQL como no SQL. Aunque me siento cómodo con estas tecnologías, no
       estoy vinculado a un único framework o tecnología.
     </p>
   </section>
 </template>
-
-<style scoped>
-.dark-mode h2,
-.dark-mode p {
-  color: #fff;
-}
-.about-container {
-  margin-top: 128px;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-}
-.about-title {
-  font-family: "Clash Display Semibold";
-  font-size: var(--text-h2);
-  color: var(--dark-100);
-}
-p {
-  font-family: "Satoshi Regular";
-  font-size: var(--text-base);
-  color: var(--dark-100);
-}
-@media screen and (max-width: 1200px) {
-  .about-title {
-    text-align: center;
-  }
-}
-</style>
