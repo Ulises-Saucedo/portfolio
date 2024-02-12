53b400b937ea96237493e6c87bdefbdc0d286526
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-12 04:36:25 -0300

    projects updated

---
+++ src/utils/projects.js
@@ -0,0 +1,104 @@
+export const projects = [
+  {
+    title: "JFK",
+    description:
+      "Página para la escuela secundaria EEST N5. Cuenta con un foro y una biblioteca de pdfs para los alumnos.",
+    technologies: [
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/html.svg",
+        alt: "html icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/css.svg",
+        alt: "css icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/js.svg",
+        alt: "javascript icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/php.svg",
+        alt: "php icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/sql.svg",
+        alt: "sql icon",
+      },
+    ],
+    links: [
+      {
+        title: "Código",
+        href: "https://github.com/Ulises-Saucedo/JFK",
+      },
+    ],
+    image: "https://i.imgur.com/eRGjXmN.jpeg",
+  },
+  {
+    title: "Arduino",
+    description:
+      "Estación meteorológica capaz de leer temperatura, humedad y presión atmosférica. Desarrollada con Arduino.",
+    technologies: [
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/html.svg",
+        alt: "html icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/css.svg",
+        alt: "css icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/js.svg",
+        alt: "javascript icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/arduino.svg",
+        alt: "arduino icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/node.svg",
+        alt: "nodejs icon",
+      },
+    ],
+    links: [
+      {
+        title: "Código",
+        href: "https://github.com/Ulises-Saucedo/Weather-Arduino",
+      },
+    ],
+    image: "https://i.imgur.com/YPEHdqb.jpeg",
+  },
+  {
+    title: "MovieMingle",
+    description:
+      "Buscador de peliculas fácil de usar. Usando la api de TMDB y Pinia para la gestión de estado.",
+    technologies: [
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/html.svg",
+        alt: "html icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/css.svg",
+        alt: "css icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/js.svg",
+        alt: "javascript icon",
+      },
+      {
+        src: "https://raw.githubusercontent.com/Ulises-Saucedo/portfolio/72b49932c75cf1037d25629ca5f3a64c6957bd92/public/technologies/vue.svg",
+        alt: "vue icon",
+      },
+    ],
+    links: [
+      {
+        title: "Código",
+        href: "https://github.com/Ulises-Saucedo/MovieMingle",
+      },
+      {
+        title: "Demo",
+        href: "https://moviemingle-five.vercel.app/",
+      },
+    ],
+    image: "https://i.imgur.com/i8O21jh.jpeg",
+  },
+];
