71c9986fe91d19350363f90a36dede492fa936b5
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-12 04:36:44 -0300

    projects component updated

--- src/components/Proyectos.vue
+++ src/components/Proyectos.vue
@@ -1,65 +1,23 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { inject } from "vue";
+import { projects } from "../utils/projects";
 import ProjectCard from "./ProjectCard.vue";
 
-const props = defineProps({
-  isDark: {
-    type: Boolean,
-  },
-});
-const projects = ref([]);
-
-onMounted(async () => {
-  const response = await fetch("./projects.json");
-  const json = await response.json();
-  projects.value = json;
-});
+const isDark = inject("isDark");
 </script>
 
 <template>
-  <section class="projects-section" :class="{ 'dark-mode': props.isDark }">
-    <h2 class="projects-title">Proyectos</h2>
-    <ProjectCard
-      v-for="project in projects"
-      :image="project.image"
-      :title="project.title"
-      :description="project.description"
-      :images="project.images"
-      :links="project.links"
-      :isDark="props.isDark"
-    />
+  <section>
+    <h2
+      class="text-center mt-60 mb-8 font-clashDisplayMedium text-h2 text-[#2F3037]"
+      :class="{ 'text-[#fff]': isDark }"
+    >
+      Proyectos
+    </h2>
+    <div
+      class="grid grid-cols-1 lg:grid-cols-3 max-w-[600px] mx-auto lg:max-w-full gap-8 p-8"
+    >
+      <ProjectCard v-for="project in projects" :project="project" />
+    </div>
   </section>
 </template>
-
-<style scoped>
-.dark-mode .projects-title {
-  color: #fff;
-}
-.projects-section {
-  margin-top: 256px;
-  display: flex;
-  flex-direction: column;
-  gap: 48px;
-}
-.projects-title {
-  font-family: "Clash Display Semibold";
-  font-size: var(--text-h2);
-  color: var(--dark-100);
-}
-@media screen and (max-width: 1200px) {
-  .projects-section {
-    margin-top: 128px;
-    gap: 32px;
-  }
-  .projects-title {
-    font-size: var(--text-h4);
-    text-align: center;
-  }
-}
-@media screen and (max-width: 800px) {
-  .projects-section {
-    margin-top: 96px;
-    gap: 24px;
-  }
-}
-</style>
